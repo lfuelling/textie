ace9b7b31d0a5c0d748d674d35c9fdfd24d43a19
lfuelling <lukas@k40s.net>
2014-09-02 13:13:52 +0200

    Basisdaten hinzugefügt

---
+++ Textie.java
@@ -0,0 +1,16 @@
+/*
+ *  TEXTIE
+ *
+ *  Wenn man einen Raum Startet (RaumX.start();), muss dieser Raum ein Array zurück geben.
+ *  Dieses Array beinhaltet im ersten Feld, ob das Level beendet wurde, und in den übrigen fünf Feldern sind die ID's der Items enthalten, die man mit sich trägt.
+ *  Wenn der Raum abgeschlossen wurde, muss das erste Feld "0" enthalten und wenn der Spieler tot ist, muss dieses Feld 1 enthalten.
+ */
+
+import java.io.Console;
+
+public class Textie {
+  public static void main (String[] args) {
+    int[6] inventory; 
+    Raum1.start();
+  }
+}
