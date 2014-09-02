df17b8159929c4d07b7f69fe42bbd24fe291386c
lfuelling <lukas@k40s.net>
2014-09-02 16:47:59 +0200

    .

--- Raum2.java
+++ Raum2.java
@@ -3,7 +3,7 @@ import java.io.Console;
 public class Raum2 {
   public static void start(int[] inventory) {
     Textie.listInventory();
-    System.out.println("Du gehst eine Steintreppe hinauf und kommst in einen weiteren dunklen Raum.");
+    System.out.println("Du kommst in einen weiteren dunklen Raum.");
 
   }
 }
