20d707689db25cd8bc03be518c4a9440a7299d38
lfuelling <lukas@k40s.net>
2014-09-02 15:56:47 +0200

    Fixed Typos

--- Raum1.java
+++ Raum1.java
@@ -6,8 +6,8 @@ public class Raum1 {
     System.out.println("Du befindest dich in einem dunklen Raum.");
     do{
       Console console = System.console();
-      command = console.readLine("Was willst du tun? ");
-      parsed_command = parseInput(command);
+      String command = console.readLine("Was willst du tun? ");
+      String[] parsed_command = Textie.parseInput(command);
       switch(parsed_command[0]){
         case "hilfe":
           System.out.println("Mögliche Befehle:");
@@ -15,11 +15,12 @@ public class Raum1 {
           System.out.println("\tnimm [gegenstand] -> Gegenstand zum Inventar hinzufügen");
           System.out.println("\tbenutze [gegenstand] -> Gegenstand benutzen");
           System.out.println("\tuntersuche [gegenstand/raum/inventar] -> Gegenstand, Raum oder Inventar untersuchen");
-          System.out.println("\tvernichte [gegenstand] -> Gegenstand aus dem Inventar löschen")
+          System.out.println("\tvernichte [gegenstand] -> Gegenstand aus dem Inventar löschen");
           break;
         case "nimm":
 
+
       }
-    }while(finished == false)
+    }while(finished == false);
   }
 }
