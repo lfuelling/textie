67daacf3f0431d55b530a343a9f01f07d8adb548
Lukas Fülling <lukas@k40s.net>
2014-09-24 18:02:25 +0200

    removeItem implementiert

--- src/main/java/de/micromata/azubi/StorageItem.java
+++ src/main/java/de/micromata/azubi/StorageItem.java
@@ -1,19 +1,34 @@
 package de.micromata.azubi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by lfuelling on 24.09.14.
  */
 public class StorageItem extends Item {
-    public StorageItem(String name, String untersucheText, String benutzeText, boolean pickable) {
+
+    protected List<Item> items = new ArrayList<Item>();
+
+    public StorageItem(String name, String untersucheText, String benutzeText, boolean pickable, Item... items) {
         super(name, untersucheText, benutzeText, pickable);
+        for (Item item : items) {
+            this.items.add(item);
+        }
     }
 
     public boolean hasItem (Item item) {
         //TODO: returns false until implemented.
+
         return false;
     }
 
     public void removeItem (Item item) {
-        //TODO: Needs implementation.
+        public boolean removeItem(Item item) {
+            if(items.remove(item))
+                return true;
+            return false;
+        }
+
     }
 }
