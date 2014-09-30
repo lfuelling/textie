45cc24eb1bcfbedff7029a31b9f7b047b0c85936
Lukas Fülling <lukas@k40s.net>
2014-09-30 15:17:57 +0200

    Exception gefangen

--- src/main/java/de/micromata/azubi/Textie.java
+++ src/main/java/de/micromata/azubi/Textie.java
@@ -7,12 +7,19 @@ package de.micromata.azubi;
 
 public class Textie {
     public static boolean diag;
+
     public static void main(String[] args) {
-        if(args[0].equals("--diag")){
-            diag = true;
-        } else {
+
+        try {
+            if(args[0].equals("--diag")){
+                diag = true;
+            } else {
+                diag = false;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
             diag = false;
         }
+
         Dungeon.getDungeon().runGame(true);
         System.exit(0);
     }
