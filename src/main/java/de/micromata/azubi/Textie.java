5796129ab6e84e2c58c8f99168839acf279247d4
Lukas Fülling <lukas@k40s.net>
2014-09-30 15:05:24 +0200

    Diag wird nun abgefragt

--- src/main/java/de/micromata/azubi/Textie.java
+++ src/main/java/de/micromata/azubi/Textie.java
@@ -6,14 +6,14 @@ package de.micromata.azubi;
  */
 
 public class Textie {
-    public static boolean diag = false;
+    public static boolean diag;
     public static void main(String[] args) {
-        Dungeon.getDungeon().runGame(true);
-        System.exit(0);
-
         if(args[0].equals("--diag")){
             diag = true;
+        } else {
+            diag = false;
         }
-
+        Dungeon.getDungeon().runGame(true);
+        System.exit(0);
     }
 }
