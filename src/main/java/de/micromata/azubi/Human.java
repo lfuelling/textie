5866103079332bbd7ea1b29e4fcf45a0fdca6b1e
Lukas Fülling <lukas@k40s.net>
2014-09-30 14:59:30 +0200

    Reward Item Einkommentiert ⚡️

--- src/main/java/de/micromata/azubi/Human.java
+++ src/main/java/de/micromata/azubi/Human.java
@@ -21,8 +21,8 @@ public class Human {
     private boolean giveItem = false;
     //ArrayList<Item> questItems;
     Item questItem;
-   // Item rewarditem;
-   // String questItemName;
+    Item rewarditem;
+    // String questItemName;
 
     public Human(String name, String dialog1, String dialog2, String questText, String questDoneText, Item rewardItem, Item questItem) {
         this.name = name;
@@ -34,7 +34,7 @@ public class Human {
       /*  for(Item questItems : questItem){
             this.questItems.add(questItems);
         }*/
-       // this.questItemName = questItem.getName();
+        // this.questItemName = questItem.getName();
         this.rewarditem = rewardItem;
     }
 
@@ -69,27 +69,26 @@ public class Human {
         }
     }
 
-
     public void doGeben(String[] parsed_command, int count) {
-            if (count == 2) {
-                String itemToUse = IOUtils.convertToName(parsed_command[1]);
-                if (itemToUse.equals(questItems)) {
-                    if (Dungeon.getDungeon().inventory.giveItem(Dungeon.getDungeon().itemMap.get(parsed_command[1].toUpperCase()))) {
-                        System.out.println(questDoneText);
-                        questDone = true;
-                        if (Dungeon.getDungeon().inventory.recieveItem(rewarditem)) {
-                            System.out.println("Im Gegenzug bekommst du von mir auch etwas. Bitteschön.");
-                        } else {
-                            System.out.println("Dein Inventar ist leider voll. Komm wieder, wenn du Platz hast.");
-                            giveItem = true;
-                        }
+        if(count == 2) {
+            String itemToUse = IOUtils.convertToName(parsed_command[1]);
+            if(itemToUse.equals(questItems)) {
+                if(Dungeon.getDungeon().inventory.giveItem(Dungeon.getDungeon().itemMap.get(parsed_command[1].toUpperCase()))) {
+                    System.out.println(questDoneText);
+                    questDone = true;
+                    if(Dungeon.getDungeon().inventory.recieveItem(rewarditem)) {
+                        System.out.println("Im Gegenzug bekommst du von mir auch etwas. Bitteschön.");
                     } else {
-                        System.out.println("Item nicht im Inventar.");
+                        System.out.println("Dein Inventar ist leider voll. Komm wieder, wenn du Platz hast.");
+                        giveItem = true;
                     }
                 } else {
-                    System.out.println("Das brauche ich nicht.");
+                    System.out.println("Item nicht im Inventar.");
                 }
+            } else {
+                System.out.println("Das brauche ich nicht.");
             }
+        }
     }
 
 }
