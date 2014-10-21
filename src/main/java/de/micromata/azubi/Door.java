8450167ceaf6386e062a462e7a2a1ff69954e2b4
Lukas Fülling <lfuelling@micromata.de>
2014-10-21 12:23:53 +0200

    Implemented door.

---
+++ src/main/java/de/micromata/azubi/Door.java
@@ -0,0 +1,56 @@
+package de.micromata.azubi;
+
+/**
+ * This defines a simple door.
+ *
+ * @author Lukas Fülling
+ * @since 21.10.2014
+ * @version 1.0
+ */
+public class Door {
+    int uid;
+    Richtung richtungRaum1;
+    Richtung richtungRaum2;
+    Raum raum1;
+    Raum raum2;
+    boolean locked;
+
+    /**
+     *
+     * @param UID The unique identifier we can use to define a key or a switch to a certain door.
+     * @param richtung The orientation the door should have in the room.
+     * @param raum1 The room the door is placed in.
+     * @param raum2 The room the door leads to. (and where it's also placed in.
+     * @param initialLockState Defines if the door is locked at game launch.
+     */
+    public Door(int UID, Richtung richtung, Raum raum1, Raum raum2, boolean initialLockState) {
+        this.uid = UID;
+        this.richtungRaum1 = richtung;
+        this.raum1 = raum1;
+        this.raum2 = raum2;
+        this.locked = initialLockState;
+        richtungRaum2 = Richtung.getOpposite(richtungRaum1);
+    }
+
+    /**
+     * Exits the current room and enters the new one.
+     */
+    public void open(){
+        if()
+        raum1.setLeaveRoom(true);
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setLocked(boolean locked) {
+
+        this.locked = locked;
+    }
+
+    public boolean isLocked() {
+
+        return locked;
+    }
+}
--- src/main/java/de/micromata/azubi/Richtung.java
+++ src/main/java/de/micromata/azubi/Richtung.java
@@ -24,4 +24,31 @@ public enum Richtung {
             return OST;
         }
     }
+
+    /**
+     *
+     * @param richtung Richtung deren Gegenteil man haben will.
+     * @return Gegenteilige Richtung
+     *
+     */
+    public static Richtung getOpposite(Richtung richtung){
+        Richtung opposite;
+        switch(richtung){
+            case NORD:
+                opposite = SUED;
+                break;
+            case SUED:
+                opposite = NORD;
+                break;
+            case WEST:
+                opposite = OST;
+                break;
+            case OST:
+                opposite = WEST;
+                break;
+            default:
+                opposite = null;
+        }
+        return opposite;
+    }
 }
